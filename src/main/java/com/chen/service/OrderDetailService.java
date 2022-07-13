d43cf8fd1fd6d5882eb5bbf63cf594e61d2b07f0
username <email>
2022-07-13 18:31:59 +0800

    瑞吉外卖项目前后台完成

--- src/main/java/com/chen/service/OrderDetailService.java
+++ src/main/java/com/chen/service/OrderDetailService.java
@@ -6,6 +6,5 @@ import com.chen.domain.OrderDetail;
 import java.util.List;
 
 public interface OrderDetailService extends IService<OrderDetail> {
-
     List<OrderDetail> getOrderDetailsByOrderId(Long orderId);
 }
